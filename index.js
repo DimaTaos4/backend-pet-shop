f737dd04780fd2b2bff130c17dd4f5dbfd28c577
Dima <taominhquan2004@gmail.com>
2025-08-23 12:14:59 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { request } = require('express');
 const express = require('express');
 const categories = require('./routes/categories');
@@ -8,7 +9,7 @@ const sequelize = require('./database/database');
 const cors = require('cors')
 const Category = require('./database/models/category');
 const Product = require('./database/models/product');
-const PORT = 3333;
+const PORT = process.env.PORT;
 
 Category.hasMany(Product);
 
